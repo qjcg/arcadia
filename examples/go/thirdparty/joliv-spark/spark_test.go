f73cb4e20139845950b042f131a77c44cb37b389
John Gosset <john@gossetx.com>
2025-09-21 16:54:31 -0500

    style: Run gofumpt

--- examples/go/thirdparty/joliv-spark/spark_test.go
+++ examples/go/thirdparty/joliv-spark/spark_test.go
@@ -7,17 +7,16 @@ import (
 )
 
 func TestLine(t *testing.T) {
-	testData := []struct{
+	testData := []struct {
 		description string
-		data []float64
-		want string
+		data        []float64
+		want        string
 	}{
 		{"empty", []float64{}, ""},
 		{"basic-ascending", []float64{1, 2, 3, 4, 5, 6, 7, 8}, "▁▂▃▄▅▆▇█"},
 		{"basic-descending", []float64{8, 7, 6, 5, 4, 3, 2, 1}, "█▇▆▅▄▃▂▁"},
 		{"basic-trio", []float64{1, 2, 3}, "▁▅█"},
 		{"temps", []float64{67, 71, 77, 85, 95, 104, 106, 105, 100, 89, 76, 66}, "▁▂▃▄▆███▇▅▃▁"},
-
 	}
 
 	for _, tc := range testData {
@@ -29,7 +28,6 @@ func TestLine(t *testing.T) {
 			}
 
 			t.Logf("data %v, sparkline %q", tc.data, got)
-
 		})
 	}
 }
